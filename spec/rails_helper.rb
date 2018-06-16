446341014af0c487afbf21b5c850db8d5a445a59
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2018-06-16 15:30:38 -0300

    Adds SimpleCov configure

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'simplecov'
+SimpleCov.start
+
+
 
 ActiveRecord::Migration.maintain_test_schema!
 
