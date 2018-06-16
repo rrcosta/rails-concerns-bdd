c81202c203672e388c9e8dff1bca2ca160f0bdac
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2018-06-16 17:22:20 -0300

    Changes root to

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :thoughts
   devise_for :users
+
+  root to: "thoughts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
